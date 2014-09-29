54db9949554e121f28a472900458a1e0a1e60b3a
alexfakhri <alexfakhri@me.com>
2014-09-29 19:03:54 +0100

    Menu created with 5 items

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,11 +3,27 @@ class Menu
 	attr_reader :price
 
 	def initialize(price)
-		@price = 0
+		@price = price
 	end
 
 	def self.burger
 		new 3.00
+	end
+
+	def self.chips
+		new 1.50
+	end
+
+	def self.chicken_kebab
+		new 4.00
+	end
+
+	def self.chicken_wrap
+		new 3.50
+	end
+
+	def self.fried_chicken
+		new 2.90
 	end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,8 +6,33 @@ describe Menu do
 	
 	it "Can create a dish with price" do
 		expect(menu.price).to eq 0
-
 	end
+
+	it "has a burger on the menu with price" do
+		burger = Menu.burger
+		expect(burger.price).to eq (3.00)
+	end
+
+	it "has chips on the menu with price" do
+		chips = Menu.chips
+		expect(chips.price).to eq (1.50)
+	end
+
+	it "has chicken kebab on the menu with price" do
+		chicken_kebab = Menu.chicken_kebab
+		expect(chicken_kebab.price).to eq (4.00)
+	end
+
+	it "has chicken wrap on the menu with price" do
+		chicken_wrap = Menu.chicken_wrap
+		expect(chicken_wrap.price).to eq (3.50)
+	end
+
+	it "has fried chicken in the menu with price" do
+		fried_chicken = Menu.fried_chicken
+		expect(fried_chicken.price).to eq (2.90)
+	end
+
 
 
 end
